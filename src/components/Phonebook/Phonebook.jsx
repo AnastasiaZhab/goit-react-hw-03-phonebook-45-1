7ba4a0a4293df351c6d25e4b2039f025bf6af9be
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2022-08-22 12:07:32 +0300

    fixed correction 2

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -9,6 +9,22 @@ class Phonebook extends Component {
         filter: ''
     }
 
+    componentDidMount() {
+        const contacts = localStorage.getItem('contacts');
+        const parseContacts = JSON.parse(contacts);
+
+        if (parseContacts) {
+            this.setState({contacts: parseContacts });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (this.state.contacts !== prevState.contacts) {
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+          }
+    }
+
     handleCreateContact = (contact) => {
         const { contacts } = this.state;
 
@@ -38,21 +54,6 @@ class Phonebook extends Component {
         )
     }
 
-    componentDidMount() {
-        const contacts = localStorage.getItem('contacts');
-        const parseContacts = JSON.parse(contacts);
-
-        if (parseContacts) {
-            this.setState({contacts: parseContacts });
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-        if (this.state.contacts !== prevState.contacts) {
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-          }
-    }
 
     render() {
         return (
